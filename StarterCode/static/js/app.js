80cd43bfd9661f43cfd73b2f4e6705c0ff2eb2c4
Ashley Anderson <151413928+AshleyKAnderson@users.noreply.github.com>
2024-04-10 12:33:33 -0500

    Delete StarterCode directory

--- StarterCode/static/js/app.js
+++
@@ -1,8 +0,0 @@
-// Get the data of samples 
-const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
-
-// Fetch the JSON data
-let data = d3.json(url).then(function(data) {
-  console.log(data);
-};
-
